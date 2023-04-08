2326d3ac20d800d0bf06b0052482c03dbfd8fab9
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-08 10:16:43 -0400

    fix lint

--- js/src/js/index.js
+++ js/src/js/index.js
@@ -1,3 +1,7 @@
+import "../css/index.css";
+
+import * as wasm from "../../dist/pkg/transports";
+
 export * from "./client";
 export * from "./connection";
 export * from "./handler";
@@ -6,13 +10,8 @@ export * from "./model";
 export * from "./transports";
 export * from "./update";
 
-import "../css/index.css";
-
-import * as wasm from "../../dist/pkg/transports";
-
 export * as wasm from "../../dist/pkg/transports";
 
 export const placeholder = "";
 
 export const foo = () => wasm.foo();
-
